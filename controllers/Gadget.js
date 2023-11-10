4b1319b58041aacddd3c3a2deee9ca7c1553f748
RevathiGuduru123 <RevathiGuduru123@users.noreply.github.com>
2023-11-10 01:05:40 -0600

    commit

--- controllers/Gadget.js
+++ controllers/Gadget.js
@@ -1,5 +1,5 @@
 var Gadget = require('../models/GadgetSchema');
-// List of all Costumes
+// List of all Gadgets
 exports.Gadget_list = async function(req, res) {
     try{
         theGadgets = await Gadget.find();
@@ -10,20 +10,28 @@ exports.Gadget_list = async function(req, res) {
         res.send(`{"error": ${err}}`);
         } 
 };
-// for a specific Costume.
-exports.Gadget_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Costume detail: ' + req.params.id);
-};
-// Handle Costume create on POST.
+// for a specific Gadget.
+exports.Gadget_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Gadget.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    
+// Handle Gadget create on POST.
 exports.Gadget_create_post = async function(req, res) {
     console.log(req.body)
     let document = new Gadget();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"costume_type":"goat", "cost":12, "size":"large"}
+    // {"Gadget_name":"goat", "type":12, "price":"large"}
     document.name = req.body.name;
-    document.type = req.body.type;
+    document.name = req.body.name;
     document.price = req.body.price;
     try{
     let result = await document.save();
@@ -34,14 +42,14 @@ exports.Gadget_create_post = async function(req, res) {
     res.send(`{"error": ${err}}`);
     } ;
 };
-// Handle Costume delete form on DELETE.
+// Handle Gadget delete form on DELETE.
 exports.Gadget_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: Costume delete DELETE ' + req.params.id);
+res.send('NOT IMPLEMENTED: Gadget delete DELETE ' + req.params.id);
 };
-// Handle Costume update form on PUT.
-exports.Gadget_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Costume update PUT' + req.params.id);
-};
+//Handle Gadget update form on PUT.
+exports.vehicles_update_put = function(req, res) {
+    res.send('NOT IMPLEMENTED: vehicles update PUT' + req.params.id);
+    };
  
 // VIEWS
 // Handle a show all view
@@ -57,3 +65,4 @@ exports.fruit_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
 }
+
